44a13a8a79711ea77f988682bc2149f532bf2242
Reed Miller <nile@techimpact.org>
2022-10-09 16:00:00 -0400

    pass all tests

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,8 +1,11 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
@@ -11,11 +14,22 @@ public class Skill extends AbstractEntity {
     @Size(max = 500, message = "Description must be no longer than 500 characters.")
     private String description;
 
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs = new ArrayList<>();
+
     public Skill(String description) {
         this.description = description;
     }
 
     public Skill() {}
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 
     public String getDescription() {
         return description;
